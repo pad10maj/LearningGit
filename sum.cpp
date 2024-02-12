3db3662f70c621198eb7c494048133dd5e4426aa
Padmaj Ulhas Naik <emb-padmnai@embitel.com>
2024-02-12 18:27:22 +0530

    last commit

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+    int a,b,add;
+    std::cout<<"Enter the value for a=";
+    std::cin>>a;
+    std::cout<<"Enter the value for b=";
+    std::cin>>b;
+    add=a+b;
+    std::cout<<"Sum="<<add;
+    return 0;
+}
